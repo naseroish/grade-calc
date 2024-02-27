1db3892ed1561e6230c059fc9da2aa63a7a54f5d
naseroish <82692921+naseroish@users.noreply.github.com>
2024-02-27 21:25:57 +0000

    Refactor supabaseConfig.ts to handle different environments

--- src/services/supabaseConfig.ts
+++ src/services/supabaseConfig.ts
@@ -1,9 +1,16 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
+let supabaseUrl: string;
+let supabaseAnonKey: string;
 
-
-const supabaseUrl= import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey= import.meta.env.VITE_SUPABASE_KEY;
-
+if (process.env.NODE_ENV === 'production') {
+    // In production (Azure)
+    supabaseUrl = process.env.VITE_SUPABASE_URL;
+    supabaseAnonKey = process.env.VITE_SUPABASE_KEY;
+  } else {
+    // In development (local)
+    supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+    supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
+  }
 
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
