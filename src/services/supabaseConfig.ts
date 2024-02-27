eac386d37d05d46a270f215eaaee8b1d9c8727c8
naseroish <82692921+naseroish@users.noreply.github.com>
2024-02-27 22:42:24 +0000

    Update Supabase configuration

--- src/services/supabaseConfig.ts
+++ src/services/supabaseConfig.ts
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY as string;
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.VITE_SUPABASE_KEY as string;
 
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
