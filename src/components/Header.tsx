9bc76c5fccc59de4ff756a5f55fa33ae25082bc5
naseroish <82692921+naseroish@users.noreply.github.com>
2024-03-18 00:40:55 +0000

    Delete unused images and update loading indicators

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import GradeLogo from '../assets/gradianceblack.png';
 
 const navigation = [
   { name: '', href: '#' },
@@ -11,81 +10,79 @@ const navigation = [
 ]
 
 export default function Header() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
     <header className="absolute inset-x-0 top-0 z-50">
-    <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-      <div className="flex lg:flex-1">
-        <a href="#" className="-m-1.5 p-1.5">
-          <span className="sr-only">Your Company</span>
-          <img
-            className="h-16 w-auto"
-            src={GradeLogo}
-            alt=""
-          />
-        </a>
-      </div>
-      <div className="flex lg:hidden">
-        <button
-          type="button"
-          className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          onClick={() => setMobileMenuOpen(true)}
-        >
-          
-          <span className="sr-only">Open main menu</span>
-          <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-        </button>
-      </div>
-      
-      <div className="hidden lg:flex lg:gap-x-12">
-        {navigation.map((item) => (
-          <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
-            {item.name}
+      <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <svg width="50" height="50" viewBox="0 0 145 110" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M83.5 6L72.5 0L0.5 38.5L72.5 77.5L144.5 39L103 16L67 35L78 41.5L103 28L122 39L72.5 65.5L23 38.5L83.5 6Z" fill="#2e3440" />
+              <path d="M18 81V60.5L27 65.5V74.5L72.5 99L117 75V65.5L126 61V81L72.5 110L18 81Z" fill="#2e3440" />
+            </svg>
           </a>
-        ))}
-      </div>
-      <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a href="/login" className="text-sm font-semibold leading-6 text-gray-900">
-          Log in <span aria-hidden="true">&rarr;</span>
-        </a>
-      </div>
-    </nav>
-    <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-      <div className="fixed inset-0 z-50" />
-      <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-  <div className="flex items-center justify-between">
-    <a href="#" className="-m-1.5 p-1.5">
-      <span className="sr-only">Your Company</span>
-      <img
-        className="h-16 w-auto"
-        src={GradeLogo}
-        alt=""
-      />
-    </a>
-    <button
-      type="button"
-      className="-m-2.5 rounded-md p-2.5 text-gray-700"
-      onClick={() => setMobileMenuOpen(false)}
-    >
-      <span className="sr-only">Close menu</span>
-      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-    </button>
-  </div>
-  <div className="mt-6 flow-root">
-    <div className="-my-6 divide-y divide-gray-500/10">
-      <div className="py-6">
-        <a
-          href="/login"
-          className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-        >
-          Log in
-        </a>
-      </div>
-    </div>
-  </div>
-</Dialog.Panel>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
 
-    </Dialog>
-  </header>
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="/login" className="text-sm font-semibold leading-6 text-gray-900">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </nav>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-50" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <svg width="50" height="50" viewBox="0 0 145 110" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M83.5 6L72.5 0L0.5 38.5L72.5 77.5L144.5 39L103 16L67 35L78 41.5L103 28L122 39L72.5 65.5L23 38.5L83.5 6Z" fill="#2e3440" />
+                <path d="M18 81V60.5L27 65.5V74.5L72.5 99L117 75V65.5L126 61V81L72.5 110L18 81Z" fill="#2e3440" />
+              </svg>
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="py-6">
+                <a
+                  href="/login"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+
+      </Dialog>
+    </header>
   );
 }
--- src/components/HomeNav.tsx
+++ src/components/HomeNav.tsx
@@ -6,21 +6,25 @@ const HomeNav = () => {
     //dark mode state
     const [theme, setTheme] = useState<string>('');
     const navigate = useNavigate();
+
     //get current theme
     useEffect(() => {
-        const currentTheme = document.documentElement.getAttribute('data-theme');
-        setTheme(currentTheme ?? '');
+        const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        const currentTheme = localStorage.getItem('theme') || (prefersDarkScheme ? 'night' : 'nord');
+        document.documentElement.setAttribute('data-theme', currentTheme);
+        setTheme(currentTheme);
     }, []);
+
     //dark mode toggle
     const toggleTheme = () => {
         const newTheme = theme === 'nord' ? 'night' : 'nord';
         document.documentElement.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
         setTheme(newTheme);
     };
 
     //tailwindcss dark mode
-    // const theme = document.documentElement.getAttribute('data-theme');
-    const isDarkTheme =  theme === 'night';
+    const isDarkTheme = theme === 'night';
     const isLightTheme = theme === 'nord';
 
     //supabase logout
@@ -37,16 +41,29 @@ const HomeNav = () => {
         navigate('/profile');
     };
 
+    const Dashboard = () => {
+        navigate('/dashboard');
+    };
+
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
-                <a className='btn btn-ghost text-xl' href='/dashboard'>
+                <a className='btn btn-ghost text-xl' onClick={Dashboard}>
                     {isDarkTheme ? (
-                        <img src='src/assets/gradiancewhite.png' className='h-12'></img>
+                        <svg width="50" height="50" viewBox="0 0 145 110" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M83.5 6L72.5 0L0.5 38.5L72.5 77.5L144.5 39L103 16L67 35L78 41.5L103 28L122 39L72.5 65.5L23 38.5L83.5 6Z" fill="#c8cbd0" />
+                            <path d="M18 81V60.5L27 65.5V74.5L72.5 99L117 75V65.5L126 61V81L72.5 110L18 81Z" fill="#c8cbd0" />
+                        </svg>
                     ) : isLightTheme ? (
-                        <img src='src/assets/gradianceblack.png' className='h-12'></img>
+                        <svg width="50" height="50" viewBox="0 0 145 110" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M83.5 6L72.5 0L0.5 38.5L72.5 77.5L144.5 39L103 16L67 35L78 41.5L103 28L122 39L72.5 65.5L23 38.5L83.5 6Z" fill="#2e3440" />
+                            <path d="M18 81V60.5L27 65.5V74.5L72.5 99L117 75V65.5L126 61V81L72.5 110L18 81Z" fill="#2e3440" />
+                        </svg>
                     ) : (
-                        <img src='src/assets/gradiancewhite.png' className='h-12'></img>
+                        <svg width="50" height="50" viewBox="0 0 145 110" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M83.5 6L72.5 0L0.5 38.5L72.5 77.5L144.5 39L103 16L67 35L78 41.5L103 28L122 39L72.5 65.5L23 38.5L83.5 6Z" fill="#2e3440" />
+                            <path d="M18 81V60.5L27 65.5V74.5L72.5 99L117 75V65.5L126 61V81L72.5 110L18 81Z" fill="#2e3440" />
+                        </svg>
                     )}
                 </a>
             </div>
@@ -60,10 +77,10 @@ const HomeNav = () => {
 
                             <input type="checkbox" onClick={toggleTheme} />
 
-                    
+
                             <svg className="swap-on fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
 
-  
+
                             <svg className="swap-off fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
 
                         </label>
